f9c8f87ea1b10bc668ecd66bd0786b2140f2d2bf
rohit20001221 <rohit20001221@gmail.com>
2020-09-15 11:38:28 +0530

    comleted clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import Login from "./components/Login";
+import Payment from "./components/Payment";
+import Tost from "./components/Tost";
+import Orders from "./components/Orders";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // useHistory,
+  Link,
+} from "react-router-dom";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HQ7uiIC8RKZ0HR2m8IH01wJX9NooZNFGPVJ68Vzr8E7zkU2uyMawuQr1uvkLBMNN7TcDCG1XZgvLz1PiIRnAUsG00Zi1zHRx6"
+);
 
 function App() {
+  // eslint-disable-next-line
+  const [{ user }, dispatch] = useStateValue();
+  // const history = useHistory();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The USER IS >> ", authUser);
+
+      if (authUser) {
+        //user is logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        //user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Tost />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Tost />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Link to="/checkout">
+              <Tost />
+            </Link>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
