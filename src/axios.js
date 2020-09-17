14e3effb9b82e1c64b8ab0b57ca6127ec92549ed
rohit20001221 <rohit20001221@gmail.com>
2020-09-17 14:13:21 +0530

    api update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazon-clone12515.openode.io", //THE API URL {CLOUD FUNCTION}
+  baseURL: "https://murmuring-wildwood-42083.herokuapp.com/", //THE API URL {CLOUD FUNCTION}
 });
 
 export default instance;
